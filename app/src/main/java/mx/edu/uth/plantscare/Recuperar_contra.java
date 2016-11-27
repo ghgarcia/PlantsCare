aa9867cc5ea18b11a8d29f2b1527c64766c7bae7
JairMarquez <jair_marquez1@hotmail.com>
2016-11-26 23:03:55 -0600

    jair
    aun no le entiendo bien ha esta madre del git hub

---
+++ app/src/main/java/mx/edu/uth/plantscare/Recuperar_contra.java
@@ -0,0 +1,13 @@
+package mx.edu.uth.plantscare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Recuperar_contra extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recuperar_contra);
+    }
+}
